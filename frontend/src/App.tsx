853a81b7d565f8fe8663e8c05c5880ed81008891
Keyner de Avila <kddeavila3@gmail.com>
2025-10-17 16:44:26 -0500

    feat: Inicializando el frontend con Vite, React y TypeScript. Incluye estructura básica y dependencias ya instaladas.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section className="h-screen flex items-center justify-center">
+          <h1 className="text-4xl font-bold">Hello world✅</h1>
+    </section>
+  )
+}
+
+export default App
